7e2be6aaa8599e89b4d03fd8abe80ba591a7fbbc
Dilip Tate <diliptate3412gmail.com>
2023-09-24 17:54:06 +0530

    Added A Add New Calculator

--- DemoSolution/First/Program.cs
+++ DemoSolution/First/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-Console.ReadLine();
+
+using First;
+Console.WriteLine("Welcome To calculator");
+Console.WriteLine("Please Enter A First Number :");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please Enter A Second Number :");
+int b = int.Parse(Console.ReadLine());
+
+
+Calculator C = new Calculator();
+C.Add(a,b);
 
 Console.WriteLine();
